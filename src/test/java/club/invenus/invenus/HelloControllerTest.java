245de568ece698c0d9a19dc08e7a252067b565a5
Paul Zhang <me@paul-zhang.de>
2022-05-27 17:04:26 +0200

    Remove hello world example

--- src/test/java/club/invenus/invenus/HelloControllerTest.java
+++ src/test/java/club/invenus/invenus/HelloControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,15 +15,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-class HelloControllerTest {
+class BaseControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void testSayHello() throws Exception {
-        mvc.perform(get("/sayHello").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andExpect(content().string("Hello World!"));
+    public void testMain() throws Exception {
+        mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Invenus/0.0.1"));
     }
 
 }
