f3a3dde6ba6fcdff96e793bfd87e35ed14bf592d
Leon Liang <leonliang@Leons-MBP-16.fritz.box>
2022-05-27 18:33:04 +0200

    set up routing for '/' and '/events'

--- front-end/invenus/src/App.js
+++ front-end/invenus/src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/home-page/home-page.component";
+import EventsPage from "./pages/events-page/events-page.component";
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div>
+      <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/events' element={<EventsPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ front-end/invenus/src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './home-page.styles.scss';
+
+const HomePage = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default HomePage;
