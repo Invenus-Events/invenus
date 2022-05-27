01297c702d7e9cf9e042ac0a9f26548ce3bd2450
Leon Liang <leonliang@Leons-MBP-16.fritz.box>
2022-05-27 18:24:37 +0200

    structural changes to project

---
+++ front-end/invenus/src/components/event-card/event-card.component.jsx
@@ -0,0 +1,10 @@
+import './event-card.styles.scss';
+
+const EventCard = () => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default EventCard;
---
+++ front-end/invenus/src/pages/events-page/events-page.component.jsx
@@ -0,0 +1,10 @@
+import './events-page.styles.scss';
+
+const EventsPage = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default EventsPage;
