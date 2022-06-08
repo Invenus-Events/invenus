0da55919265eb0c13379ab2c746b9b3eac8e5890
Leon Liang <leonliang@Leons-MacBook-Pro-16.fritz.box>
2022-06-08 19:17:16 +0200

    bug fixes

--- front-end/invenus/src/components/events-list/events-list.component.jsx
+++ front-end/invenus/src/components/events-list/events-list.component.jsx
@@ -27,7 +27,27 @@ class EventsList extends React.Component {
             <div className='events-list-container'>
                 <div className='events-list-content'>
                     <h1>Upcoming events.</h1>
-                    <input className='events-list-search-field' type="text" placeholder="Search..."/>
+                    <div className="filter-accordion accordion">
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="headingOne">
+                                <button className="events-list-sort-button accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Sort and filter
+                                </button>
+                            </h2>
+                            <div id="collapseOne" className="accordion-collapse collapse"
+                                 aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                    <strong>This is the first item's accordion body.</strong> It is shown by default,
+                                    until the collapse plugin adds the appropriate classes that we use to style each
+                                    element. These classes control the overall appearance, as well as the showing and
+                                    hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+                                    our default variables. It's also worth noting that just about any HTML can go within
+                                    the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className={`events-list-event-listing ${this.state.isMobile ? null : 'scroll-snap'}`}>
                         {
                             this.props.events.map((event, index) => {
