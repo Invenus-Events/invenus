e4b028cc7be925f8b3ed5746cdab10f2c35c3fe5
behsadriemer <behsad.riemer@icloud.com>
2022-06-24 10:47:49 +0200

    changed clubs to club and events to event

--- client/invenus/src/components/event-page-header/event-page-header.component.jsx
+++ client/invenus/src/components/event-page-header/event-page-header.component.jsx
@@ -5,15 +5,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const EventPageHeader = ({event}) => {
     return (
-        <div className="EventPageHeader container-lg">
+        <div className="EventPageHeader container-lg ">
             <div className="row">
                 <div className="col-sm">
                     <h1 className = "EventPageHeaderTitle"> {event.title} </h1>
-                    <h2 className = "EventPageHeaderDateAndTime"> {event.date}, {event.startTime}</h2>
+                    <h2 className = "EventPageHeaderDateAndTime"> {event.date}: {event.startTime}</h2>
                 </div>
                 <div className="col-sm eventPageHeaderButtons">
                     <button className='event-page-header-button event-page-header-share'>Share</button>
                     <button className='event-page-header-button'>Join</button>
+                    <button className='event-page-header-button event-page-header-share'>Buy</button>
                 </div>
             </div>
         </div>
