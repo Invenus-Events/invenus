ec8bcb81ad1667b736632984ca9c4b220a19b708
Paul Zhang <me@paul-zhang.de>
2022-07-21 16:41:52 +0200

    remove minio from nginx

--- src/main/java/club/invenus/invenus/InvenusApplication.java
+++ src/main/java/club/invenus/invenus/InvenusApplication.java
@@ -3,8 +3,6 @@ package club.invenus.invenus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class InvenusApplication {
 
