2c5daf5017cac7b9aeeb333129453ad7dc803618
behsadriemer <behsad.riemer@icloud.com>
2022-07-21 19:52:45 +0200

    Fixed missing location address in location card component

--- client/invenus/src/components/event-location-card/event-location-card.component.jsx
+++ client/invenus/src/components/event-location-card/event-location-card.component.jsx
@@ -78,7 +78,7 @@ class EventLocationCard extends React.Component{
                              className="bi bi-geo-alt-fill bootstrap-icon" viewBox="0 0 16 16">
                             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                         </svg>
-                        {this.props.event.location.address.address}
+                        {this.props.event.location.address}
                     </p>
                 </div>
                 <div ref={this.mapContainer} className="event-location-card-map-container" />
